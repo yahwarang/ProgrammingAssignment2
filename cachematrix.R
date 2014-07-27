9a724f1abee9e44d7b92b710023a8f00686f50ec
KiSun Song <yahwarang@naver.com>
2014-07-28 00:07:37 +0900

    Adding comments.

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,20 @@
 
 ## Write a short comment describing this function
 
+# default argurment has empty matrix
 makeCacheMatrix <- function(x = matrix()) {
   inversed <- NULL
   set <- function(y) {
     x <<- y
+    # set null to 'inversed' varilable, because the matrix is renewed.
     inversed <<- NULL
   }
+  # return the matrix itself.
   get <- function() x
+  # set the 'inversed'
   setInverse <- function(solved) inversed <<- solved
   getInverse <- function() inversed
+  # return a list having four elements.
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
@@ -22,11 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   inversed <- x$getInverse()
+  # ifthere is a cached....
   if(!is.null(inversed)) {
     message("getting cached data")
     return(inversed)
   }
   matrix <- x$get()
+  # when there is no cache, we need to calculate 'inversed'.
   inversed <- solve(matrix, ...)
   x$setInverse(inversed)
   inversed
